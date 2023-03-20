b64be75fe8731b051d13a710ce9875f79916d46c
ljh37694 <ljh37694@hoon37694>
2023-03-20 20:47:43 +0900

    주석 달기(미완)

--- tetris.js
+++ tetris.js
@@ -7,40 +7,45 @@ const scoreDisplay = document.querySelector(".score");
 const restartButton = document.querySelector(".game-text > button");
 
 // Setting
-const GAME_ROWS = 20;
-const GAME_COLS = 10;
+const GAME_ROWS = 20; // 테트리스 행 개수
+const GAME_COLS = 10; // 테트리스 열 개수
 
 // Variables
-let score = 0;
-let duration = 500;
-let downInterval;
-let tempMovingItem;
+let score = 0; // 점수
+let duration = 500; // 블럭이 다시 떨어지는 데 걸리는 시간
+let downInterval; // 블럭이 떨어지는 주기
+let tempMovingItem; // 현재 블럭을 임시 저장
 
+// 현재 활성화된 블럭
 const movingItem = {
-    type: "",
-    direction: 1,
-    top: 0,
-    left: 0,
+    type: "", // 블럭의 종류
+    direction: 1, // 어느 방향으로 회전한 상태인지
+    top: 0, // 맨 위 칸부터의 거리
+    left: 0, // 맨 왼쪽 칸부터의 거리
 }
 
 init();
 
 // Functions
-function init() {
-    const blocksArray = Object.entries(BLOCKS);
-    const randomIndex = Math.floor(Math.random() * blocksArray.length);
-    movingItem.type = blocksArray[randomIndex][0];
+function init() { // 초기화
+    const blocksArray = Object.entries(BLOCKS); // 모든 종류의 블럭 배열
+    const randomIndex = Math.floor(Math.random() * blocksArray.length); // blockArray에서 랜덤으로 블럭을 선택하기 위한 변수
+    movingItem.type = blocksArray[randomIndex][0]; // 현재 블럭에 랜덤으로 1가지 타입을 부여
 
-    tempMovingItem = { ...movingItem };
+    tempMovingItem = { ...movingItem }; // 깊은 복사
     
+    // 행열의 길이에 맞춰 테트리스 맵 만들기
     for (let i = 0; i < GAME_ROWS; i++) {
         prependNewLine();
     }
 
+    // 새로운 블럭 만들기
     generateNewBlock();
+
+    console.log(playground.childNodes);
 }
 
-function prependNewLine() {
+function prependNewLine() { // 열 길이만큼 칸을 가진 라인 만들기
     const li = document.createElement("li");
     const ul = document.createElement("ul");
 
@@ -58,6 +63,7 @@ function renderBlocks(moveType = "") {
     const { type, direction, top, left } = tempMovingItem;
     const movingBlocks = document.querySelectorAll(".moving");
 
+    // 현재 블록의 moving을 없앤다.
     movingBlocks.forEach(moving => {
         moving.classList.remove(type, "moving");
     })
@@ -68,6 +74,7 @@ function renderBlocks(moveType = "") {
         const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
         const isAvailable = checkEmpty(target);
 
+        // target이 유효할 때
         if (isAvailable) {
             target.classList.add(type, "moving");
         }
@@ -97,7 +104,7 @@ function renderBlocks(moveType = "") {
     movingItem.direction = direction;
 }
 
-function seizeBlock() {
+function seizeBlock() { // 블럭이 내려갈 수 있을만큼 내려갔을 때
     const movingBlocks = document.querySelectorAll(".moving");
 
     movingBlocks.forEach(moving => {
@@ -111,6 +118,7 @@ function seizeBlock() {
 function checkMatch() {
     const childNodes = playground.childNodes;
 
+    // 테트리스의 각 줄마다 모든 칸이 seized라면 그 줄을 빈칸으로 바꾼다.
     childNodes.forEach(child => {
         let matched = true;
 
@@ -120,6 +128,7 @@ function checkMatch() {
             }
         })
 
+        // 현재 줄의 모든 칸이 seized이면 현재 줄 없애고 맨 위에 새로운 줄을 추가한다.
         if (matched) {
             child.remove();
             prependNewLine();
@@ -131,7 +140,7 @@ function checkMatch() {
     generateNewBlock();
 }
 
-function generateNewBlock() {
+function generateNewBlock() { // 새로운 블럭 생성
     clearInterval(downInterval);
 
     downInterval = setInterval(() => {
@@ -211,6 +220,7 @@ document.addEventListener("keydown", e=> {
             break;
     }
 })
+
 
 restartButton.addEventListener("click", () => {
     playground.innerHTML = "";
